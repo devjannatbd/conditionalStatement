7f5b14ce5cdb0f6d45958ec7c3bf7d62f9cdb945
jannat <devjannatbd@gmail.com>
2024-09-15 23:23:28 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,51 @@
+var persone1 ="jannatul";
+var persone2 ="islam";
+
+console.log(persone1+'' + persone2);
+
+var friuts = 200;
+var potol = '30';
+var changedPotol =parseInt(potol); 
+
+console.log(friuts + changedPotol);
+
+var price = "400.567"
+var changedValu =parseFloat(price);
+console.log(changedValu);
+var result;
+console.log (result); //undefind 
+
+// comparison
+// greter-  '>'
+// less-  '<'
+// equal - '=='
+// greter equal- '>='
+// less equal- '<='
+//not equal - '!='
+// And - '&&'
+// or - '||'
+console.log (5 > 2);
+console.log (15 < 50);
+console.log (50 != 50);
+
+console.log (50 === '50');
+console.log (50 <= 50);
+console.log (65 >= 50);
+console.log (5>2 && 3<4);
+console.log (5>2 || 3<4);
+ var alu = 50;
+if (alu == 50 ) {
+  console.log ("ami ajk alu khabo");
+}
+else {
+  console.log("ami gorib");
+}
+
+
+var khaoabikina = "khaoabo";
+if (khaoabikina ==="khaoabo") {
+  console.log ("thik ache ajk ami tor sathe khaite jabo")
+}
+else {
+  console.log ("Ajk amr pet vora")
+}
